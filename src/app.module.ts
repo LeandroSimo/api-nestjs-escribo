92b6b31fdad5c048b500b148294c13923af1c781
LeandroSimo <leo.bt.12@hotmail.com>
2023-11-23 00:02:39 -0300

    feat: adding env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,16 +8,19 @@ import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './auth/shared/constants';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './auth/shared/jwt.strategy';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: jwtConstants.secret,
       signOptions: { expiresIn: '30m' },
     }),
     MongooseModule.forRoot(
-      'mongodb+srv://escribo:VxZerjJGV8IXIR2v@escribo.jsotpsp.mongodb.net/?retryWrites=true&w=majority',
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_CLUSTER}.jsotpsp.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`,
+      // 'mongodb+srv://escribo:VxZerjJGV8IXIR2v@escribo.jsotpsp.mongodb.net/?retryWrites=true&w=majority',
     ),
     UserModule,
     AuthModule,
